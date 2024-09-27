5296238bec012e437bbc4d53c89b1f46404ed4f4
Lukas Maly <smejdil@gmail.com>
2024-09-27 10:23:56 +0200

    Fix sh script

--- Scripts/prvni_script.sh
+++ Scripts/prvni_script.sh
@@ -9,6 +9,8 @@ cat /var/log/apt/term.log | wc -l
 echo "Pocet radku vyrazu deb v logu"
 echo "---------------------------------------"
 
-cat /var/log/apt/term.log | wc -l | grep deb
+cat /var/log/apt/term.log | grep deb | wc -l
+
+echo "---------------------------------------"
 
 # EOF
