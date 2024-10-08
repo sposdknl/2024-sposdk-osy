d8a0b08d44c1c0c914430d4c1b9d312c09acbb06
Lukas Maly <smejdil@gmail.com>
2024-10-08 22:04:03 +0200

    Update script use env and use auth.log

--- Scripts/prvni_script.sh
+++ Scripts/prvni_script.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+LOG_AUTH="/var/log/auth.log"
+
 echo "Dnes je:" `date`
 
 echo "Pocet radku v logu"
 echo "---------------------------------------"
 
-cat /var/log/apt/term.log | wc -l
+cat $LOG_AUTH | wc -l
 
 echo "Pocet radku vyrazu deb v logu"
 echo "---------------------------------------"
 
-cat /var/log/apt/term.log | grep deb | wc -l
+cat $LOG_AUTH | grep ssh | wc -l
 
 echo "---------------------------------------"
 
