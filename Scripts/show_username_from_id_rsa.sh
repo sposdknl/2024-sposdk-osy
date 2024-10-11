804b1ecafad5d579aede5a23a1e1d1327645fa63
Lukas Maly <Iam@LukasMaly.NET>
2024-10-11 15:43:19 +0200

    Add SSH keys + short script

---
+++ Scripts/show_username_from_id_rsa.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Dnes je:" `date`
+
+echo "Počet ssh id_rsa.pub klicu"
+echo "---------------------------------------"
+
+ls -1 ../SSH/*.pub | wc -l
+
+echo "---------------------------------------"
+echo "Username:"
+echo "---------------------------------------"
+
+for i in `ls -1 ../SSH/`;
+  do
+     cat ../SSH/$i | awk -F'[\\\\@]' '{print $2}'
+done
+
+echo "---------------------------------------"
+
+# EOF
